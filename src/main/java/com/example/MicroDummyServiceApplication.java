345aab39077ebeb0b096b386e30200f3a85a549b
wangxianglu <lu_action@qq.com>
2017-04-12 23:02:33 +0800

    3

--- src/main/java/com/example/MicroDummyServiceApplication.java
+++ src/main/java/com/example/MicroDummyServiceApplication.java
@@ -1,29 +1,39 @@
 package com.example;
 
+import com.example.service.security.CustomUserInfoTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableResourceServer
 @EnableConfigurationProperties
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableOAuth2Sso
-public class MicroDummyServiceApplication {
+@Configuration
+public class MicroDummyServiceApplication extends ResourceServerConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroDummyServiceApplication.class, args);
 	}
+
+	@Autowired
+	private ResourceServerProperties sso;
 
 	@Bean
 	@ConfigurationProperties(prefix = "security.oauth2.client")
@@ -36,10 +46,16 @@ public class MicroDummyServiceApplication {
 		return new OAuth2RestTemplate(clientCredentialsResourceDetails());
 	}
 
-	@Configuration
-	static class CustomConversionsConfig {
+	@Bean
+	public ResourceServerTokenServices tokenServices() {
+		return new CustomUserInfoTokenService(sso.getUserInfoUri(), sso.getClientId());
+	}
 
-
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/" , "/demo").permitAll()
+				.anyRequest().authenticated();
 	}
 
 }
---
+++ src/main/java/com/example/service/AccountService.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+/**
+ * Created by L on 2017/4/12.
+ */
+public interface AccountService {
+}
